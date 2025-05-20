65bbc3768d2d7d54cf0c5fd71a72882aa4c65f81
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-20 21:32:23 +0200

    billetterie: implement email with mailtrap

--- src/main/java/com/cantet/thibaut/payment/Application.java
+++ src/main/java/com/cantet/thibaut/payment/Application.java
@@ -1,9 +1,12 @@
 package com.cantet.thibaut.payment;
 
+import com.cantet.thibaut.payment.infrastructure.service.EmailConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EmailConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/cantet/thibaut/payment/infrastructure/service/EmailConfiguration.java
@@ -0,0 +1,7 @@
+package com.cantet.thibaut.payment.infrastructure.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "email")
+public record EmailConfiguration(String host, int port, String username, String password) {
+}
