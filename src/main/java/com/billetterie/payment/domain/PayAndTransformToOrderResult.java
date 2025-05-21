3066ac03d80eeeaef3489450f816093e075e0ac3
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-21 21:45:47 +0200

    billetterie: use same PayAndTransformToOrderResult for both use cases
    fix use same

--- src/main/java/com/billetterie/payment/domain/PayAndTransformToOrderResult.java
+++ src/main/java/com/billetterie/payment/domain/PayAndTransformToOrderResult.java
@@ -1,4 +1,42 @@
 package com.billetterie.payment.domain;
 
-public record PayAndTransformToOrderResult(PaymentStatus status, String transactionId, String redirectUrl, String orderId, float amount) {
+import static com.billetterie.payment.domain.PaymentStatus.*;
+
+public record PayAndTransformToOrderResult(PaymentStatus status, String transactionId, String redirectUrl, String orderId, Float amount) {
+
+    public static PayAndTransformToOrderResult pending(String transactionId, String redirectUrl, Float amount) {
+        return new PayAndTransformToOrderResult(
+                PENDING,
+                transactionId,
+                redirectUrl,
+                null,
+                amount);
+    }
+
+    public static PayAndTransformToOrderResult failed(PaymentStatus status, String transactionId) {
+        return new PayAndTransformToOrderResult(
+                status,
+                transactionId,
+                null,
+                null,
+                0f);
+    }
+
+    public static PayAndTransformToOrderResult failed(PaymentStatus status, String transactionId, String redirectUrl) {
+        return new PayAndTransformToOrderResult(
+                status,
+                transactionId,
+                redirectUrl,
+                null,
+                0f);
+    }
+
+    public static PayAndTransformToOrderResult succeeded(String transactionId, String orderId, Float amount) {
+        return new PayAndTransformToOrderResult(
+                PaymentStatus.SUCCESS,
+                transactionId,
+                String.format("/confirmation/%s?amount=%s", orderId, amount),
+                orderId,
+                amount);
+    }
 }
