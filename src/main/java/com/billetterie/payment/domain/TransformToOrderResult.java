4fd652a7a675d25d8d9ddc015fbab8a337217658
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-21 21:45:47 +0200

    billetterie: use same PayAndTransformToOrderResult for both use cases
    fix use same

--- src/main/java/com/billetterie/payment/domain/TransformToOrderResult.java
+++
@@ -1,4 +0,0 @@
-package com.billetterie.payment.domain;
-
-public record TransformToOrderResult(TransformToOrderStatus status, String transactionId, String redirectUrl, String orderId, Float amount) {
-}
