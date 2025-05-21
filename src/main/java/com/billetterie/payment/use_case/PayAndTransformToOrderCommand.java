e513e045822174ebbe1168cc5980cb5b16413ca2
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-21 21:31:51 +0200

    billetterie: encapsulate parameters in new TransformToOrderCommand

--- src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrderCommand.java
+++ src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrderCommand.java
@@ -1,4 +1,4 @@
-package com.billetterie.payment.payment.use_case;
+package com.billetterie.payment.use_case;
 
 public record PayAndTransformToOrderCommand(String cartId, String cardNumber, String expirationDate, String cypher, float amount) {
 }
