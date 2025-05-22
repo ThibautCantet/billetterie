4d9ef34e55a0e030bc73adb45f20db43244d36bd
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-22 21:34:29 +0200

    billetterie: implement handler for CancelTransaction

--- src/main/java/com/cantet/thibaut/payment/use_case/CancelTransaction.java
+++ src/main/java/com/cantet/thibaut/payment/use_case/CancelTransaction.java
@@ -4,6 +4,8 @@ import com.cantet.thibaut.payment.common.cqrs.command.CommandHandler;
 import com.cantet.thibaut.payment.common.cqrs.command.CommandResponse;
 import com.cantet.thibaut.payment.common.cqrs.event.Event;
 import com.cantet.thibaut.payment.domain.Bank;
+import com.cantet.thibaut.payment.domain.CancelTransactionFailed;
+import com.cantet.thibaut.payment.domain.CancelTransactionSucceeded;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +17,15 @@ public class CancelTransaction implements CommandHandler<CancelTransactionComman
     }
 
     public CommandResponse<Event> execute(CancelTransactionCommand  command) {
-        var cancel = bank.cancel(command.transactionId(), command.amount());
-        return null;
+        boolean cancel = bank.cancel(command.transactionId(), command.amount());
+        if (!cancel) {
+            return new CommandResponse<>(new CancelTransactionFailed(command.transactionId(), command.cartId(), command.amount()));
+        }
+        return new CommandResponse<>(new CancelTransactionSucceeded(command.transactionId()));
     }
 
     @Override
     public Class listenTo() {
-        return null;
+        return CancelTransactionCommand.class;
     }
 }
