a3f2576c1b32e070ed9b6c7e040564095eeb1e05
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 21:38:06 +0200

    billetterie: implement OrderNotCreatedListener

--- src/main/java/com/billetterie/payment/listener/OrderNotCreatedListener.java
+++ src/main/java/com/billetterie/payment/listener/OrderNotCreatedListener.java
@@ -3,16 +3,17 @@ package com.billetterie.payment.listener;
 import com.billetterie.payment.common.cqrs.command.Command;
 import com.billetterie.payment.common.cqrs.event.EventHandlerCommand;
 import com.billetterie.payment.domain.OrderNotCreated;
+import com.billetterie.payment.use_case.CancelTransactionCommand;
 
 public class OrderNotCreatedListener extends EventHandlerCommand<OrderNotCreated> {
 
     @Override
     public Command execute(OrderNotCreated event) {
-        return null;
+        return new CancelTransactionCommand(event.transactionId(), event.cartId(), event.amount());
     }
 
     @Override
     public Class<OrderNotCreated> listenTo() {
-        return null;
+        return OrderNotCreated.class;
     }
 }
