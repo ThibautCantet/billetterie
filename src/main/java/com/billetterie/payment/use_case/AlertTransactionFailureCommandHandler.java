8f76903d725e3d7533b845c04f0633ece1e96a5e
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 15:44:51 +0200

    billetterie: 04 [unit test] for AlerteTransactionFailure command handler

---
+++ src/main/java/com/billetterie/payment/use_case/AlertTransactionFailureCommandHandler.java
@@ -0,0 +1,29 @@
+package com.billetterie.payment.use_case;
+
+import java.util.List;
+
+import com.billetterie.payment.common.cqrs.command.CommandHandler;
+import com.billetterie.payment.common.cqrs.command.CommandResponse;
+import com.billetterie.payment.common.cqrs.event.Event;
+import com.billetterie.payment.domain.CustomerSupport;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlertTransactionFailureCommandHandler implements CommandHandler<AlertTransactionFailureCommand, CommandResponse<Event>> {
+    private final CustomerSupport customerSupport;
+
+    public AlertTransactionFailureCommandHandler(CustomerSupport customerSupport) {
+        this.customerSupport = customerSupport;
+    }
+
+    public CommandResponse<Event> handle(AlertTransactionFailureCommand command) {
+        customerSupport.alertTransactionFailure(command.transactionId(), command.cartId(), command.amount());
+        //TODO: return a new CustomerSupportAlerted event
+        return new CommandResponse<>(List.of());
+    }
+
+    @Override
+    public Class listenTo() {
+        return AlertTransactionFailureCommand.class;
+    }
+}
