adf8020694215d29e72ee87c55d6269240307a0a
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 19:31:39 +0200

    billetterie: 08 [unit test] for PaymentSucceededListener

---
+++ src/main/java/com/billetterie/payment/listener/PaymentSucceededListener.java
@@ -0,0 +1,24 @@
+package com.billetterie.payment.listener;
+
+import com.billetterie.payment.common.cqrs.command.Command;
+import com.billetterie.payment.common.cqrs.event.EventHandlerCommand;
+import com.billetterie.payment.domain.PaymentSucceeded;
+import com.billetterie.payment.use_case.TransformToOrderCommand;
+
+public class PaymentSucceededListener extends EventHandlerCommand<PaymentSucceeded> {
+
+    public PaymentSucceededListener() {
+    }
+
+    @Override
+    public Command execute(PaymentSucceeded event) {
+        //TODO: return a TransformToOrderCommand
+        //TODO: then implement CommandBusFactory and PaymentController
+        return null;
+    }
+
+    @Override
+    public Class<PaymentSucceeded> listenTo() {
+        return null;
+    }
+}
