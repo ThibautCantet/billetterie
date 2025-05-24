1a2f7512e101d834922f8254cf0ff9d804bcafc0
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 21:38:06 +0200

    billetterie: implement OrderNotCreatedListener

--- src/main/java/com/cantet/thibaut/payment/listener/OrderNotCreatedListener.java
+++ src/main/java/com/cantet/thibaut/payment/listener/OrderNotCreatedListener.java
@@ -3,16 +3,17 @@ package com.cantet.thibaut.payment.listener;
 import com.cantet.thibaut.payment.common.cqrs.command.Command;
 import com.cantet.thibaut.payment.common.cqrs.event.EventHandlerCommand;
 import com.cantet.thibaut.payment.domain.OrderNotCreated;
+import com.cantet.thibaut.payment.use_case.CancelTransactionCommand;
 
 public class OrderNotCreatedListener extends EventHandlerCommand<OrderNotCreated> {
 
     @Override
     public Command execute(OrderNotCreated event) {
-        return null;
+        return new CancelTransactionCommand(event.transactionId(), event.amount(), event.redirectUrl());
     }
 
     @Override
     public Class<OrderNotCreated> listenTo() {
-        return null;
+        return OrderNotCreated.class;
     }
 }
