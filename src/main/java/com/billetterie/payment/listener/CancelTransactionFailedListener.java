7b59ee555c7c44b037e22ed5a4169fe67fb6e313
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 22:15:46 +0200

    billetterie: 10 [unit test] add CancelTransactionFailedListener

---
+++ src/main/java/com/billetterie/payment/listener/CancelTransactionFailedListener.java
@@ -0,0 +1,19 @@
+package com.billetterie.payment.listener;
+
+import com.billetterie.payment.common.cqrs.command.Command;
+import com.billetterie.payment.common.cqrs.event.EventHandlerCommand;
+import com.billetterie.payment.domain.CancelTransactionFailed;
+
+public class CancelTransactionFailedListener extends EventHandlerCommand<CancelTransactionFailed> {
+
+    @Override
+    public Command handle(CancelTransactionFailed event) {
+        //TODO: return a AlertTransactionFailureCommand
+        return null;
+    }
+
+    @Override
+    public Class<CancelTransactionFailed> listenTo() {
+        return null;
+    }
+}
