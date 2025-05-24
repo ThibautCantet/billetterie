19015c6b4bdd2f2cdc2ac818c8474580cdf2cae1
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 15:44:51 +0200

    billetterie: [unit test] for AlerteTransactionFailure handler

---
+++ src/test/java/com/billetterie/payment/use_case/AlertTransactionFailureTest.java
@@ -0,0 +1,46 @@
+package com.billetterie.payment.use_case;
+
+import com.billetterie.payment.domain.CustomerSupport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AlertTransactionFailureTest {
+
+    private AlertTransactionFailure alertTransactionFailure;
+
+    @Mock
+    private CustomerSupport customerSupport;
+
+    @BeforeEach
+    void setUp() {
+        alertTransactionFailure = new AlertTransactionFailure(customerSupport);
+    }
+
+    @Nested
+    class Execute {
+        @Test
+        void should_sent_email() {
+            var alertTransactionFailureCommand = new AlertTransactionFailureCommand("tx123", "Failed transaction alert", 100f);
+
+            alertTransactionFailure.execute(alertTransactionFailureCommand);
+
+            verify(customerSupport).alertTransactionFailure(
+                    "tx123",
+                    "Failed transaction alert",
+                    100f);
+        }
+    }
+
+    @Test
+    void listenTo_should_return_AlertTransactionFailureCommand() {
+        assertThat(alertTransactionFailure.listenTo()).isEqualTo(AlertTransactionFailureCommand.class);
+    }
+}
