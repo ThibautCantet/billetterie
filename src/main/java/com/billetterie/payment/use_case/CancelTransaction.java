1bca2703336a6b536c9b3167ae8db0ea120be768
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 15:07:38 +0200

    billetterie: 05 [unit test] for CancelTransaction handler

--- src/main/java/com/billetterie/payment/use_case/CancelTransaction.java
+++ src/main/java/com/billetterie/payment/use_case/CancelTransaction.java
@@ -1,17 +1,28 @@
 package com.billetterie.payment.use_case;
 
+import com.billetterie.payment.common.cqrs.command.CommandHandler;
+import com.billetterie.payment.common.cqrs.command.CommandResponse;
+import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.Bank;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CancelTransaction {
+public class CancelTransaction implements CommandHandler<CancelTransactionCommand, CommandResponse<Event>> {
     private final Bank bank;
 
     public CancelTransaction(Bank bank) {
         this.bank = bank;
     }
 
-    public boolean execute(CancelTransactionCommand  command) {
-        return bank.cancel(command.transactionId(), command.amount());
+    public CommandResponse<Event> execute(CancelTransactionCommand  command) {
+        var cancel = bank.cancel(command.transactionId(), command.amount());
+        //TODO: depending on the cancel result, return a CommandResponse
+        // with CancelTransactionSucceeded or CancelTransactionFailed event
+        return null;
+    }
+
+    @Override
+    public Class listenTo() {
+        return null;
     }
 }
