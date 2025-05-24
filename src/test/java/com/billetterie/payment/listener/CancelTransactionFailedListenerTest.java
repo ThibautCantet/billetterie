496bac1a09b8592d2d24a6c028a7a521d515dc28
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-24 22:15:46 +0200

    billetterie: 10 [unit test] add CancelTransactionFailedListener

---
+++ src/test/java/com/billetterie/payment/listener/CancelTransactionFailedListenerTest.java
@@ -0,0 +1,47 @@
+package com.billetterie.payment.listener;
+
+import com.billetterie.payment.domain.CancelTransactionFailed;
+import com.billetterie.payment.use_case.AlertTransactionFailureCommand;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CancelTransactionFailedListenerTest {
+
+    private static final String TRANSACTION_ID = "tx123";
+    private static final String CART_ID = "cartId";
+    private static final float AMOUNT = 100.0f;
+
+    private CancelTransactionFailedListener cancelTransactionFailedListener;
+
+    @BeforeEach
+    void setUp() {
+        cancelTransactionFailedListener = new CancelTransactionFailedListener();
+    }
+
+    @Test
+    void should_return_new_AlertTransactionFailureCommand() {
+        var cancelTransactionFailed = new CancelTransactionFailed(TRANSACTION_ID, CART_ID, AMOUNT);
+
+        var command = cancelTransactionFailedListener.handle(cancelTransactionFailed);
+
+        assertThat(command)
+                .isExactlyInstanceOf(AlertTransactionFailureCommand.class)
+                .asInstanceOf(InstanceOfAssertFactories.type(AlertTransactionFailureCommand.class))
+                .extracting(AlertTransactionFailureCommand::transactionId,
+                        AlertTransactionFailureCommand::amount)
+                .containsExactly(TRANSACTION_ID, AMOUNT);
+    }
+
+    @Test
+    void should_return_CancelTransactionFailed() {
+        var listenTo = cancelTransactionFailedListener.listenTo();
+
+        assertThat(listenTo).isEqualTo(CancelTransactionFailed.class);
+    }
+}
