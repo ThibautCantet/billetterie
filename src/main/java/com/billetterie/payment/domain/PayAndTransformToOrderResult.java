99178b64885e238110e35cbe7ad534039f74d807
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-25 21:51:52 +0200

    billetterie: implement handler for PayAndTransformToOrder

--- src/main/java/com/billetterie/payment/domain/PayAndTransformToOrderResult.java
+++
@@ -1,4 +0,0 @@
-package com.billetterie.payment.domain;
-
-public record PayAndTransformToOrderResult(PaymentStatus status, String transactionId, String redirectUrl, String orderId, Float amount) {
-}
