c73e5981b19b6198ff980b25a7be544f33c00ee9
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-25 21:58:33 +0200

    billetterie: [unit test] for Pay handler

--- src/main/java/com/billetterie/payment/use_case/Pay.java
+++ src/main/java/com/billetterie/payment/use_case/Pay.java
@@ -1,20 +1,29 @@
 package com.billetterie.payment.use_case;
 
+import com.billetterie.payment.common.cqrs.command.CommandHandler;
+import com.billetterie.payment.common.cqrs.command.CommandResponse;
+import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.Bank;
 import com.billetterie.payment.domain.Payment;
-import com.billetterie.payment.domain.Transaction;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Pay {
+public class Pay implements CommandHandler<PayCommand, CommandResponse<Event>> {
     private final Bank bank;
 
     public Pay(Bank bank) {
         this.bank = bank;
     }
 
-    public Transaction execute(PayCommand command) {
+    public CommandResponse<Event> execute(PayCommand command) {
         Payment payment = new Payment(command.cardNumber(), command.expirationDate(), command.cypher(), command.cartId(), command.amount());
-        return bank.pay(payment);
+        var transaction = bank.pay(payment);
+
+        return null;
+    }
+
+    @Override
+    public Class listenTo() {
+        return null;
     }
 }
