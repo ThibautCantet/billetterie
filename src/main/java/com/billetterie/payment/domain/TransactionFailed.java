5f60887c84ca315f1d028854c95434e16395146f
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-25 21:58:33 +0200

    billetterie: 07 [unit test] for Pay command handler

--- src/main/java/com/billetterie/payment/domain/TransactionFailed.java
+++ src/main/java/com/billetterie/payment/domain/TransactionFailed.java
@@ -3,4 +3,7 @@ package com.billetterie.payment.domain;
 import com.billetterie.payment.common.cqrs.event.Event;
 
 public record TransactionFailed(PaymentStatus status, String id) implements Event {
+    public static TransactionFailed of(String transactionId) {
+        return new TransactionFailed(PaymentStatus.FAILED, transactionId);
+    }
 }
