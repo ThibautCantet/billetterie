c600da819fb00eaaf9bbb8bbd7c22c447fb71396
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-25 22:11:32 +0200

    billetterie: implement Pay handler

--- src/main/java/com/billetterie/payment/use_case/Pay.java
+++ src/main/java/com/billetterie/payment/use_case/Pay.java
@@ -5,10 +5,16 @@ import com.billetterie.payment.common.cqrs.command.CommandResponse;
 import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.Bank;
 import com.billetterie.payment.domain.Payment;
+import com.billetterie.payment.domain.PaymentSucceeded;
+import com.billetterie.payment.domain.TransactionFailed;
+import com.billetterie.payment.domain.ValidationRequested;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Pay implements CommandHandler<PayCommand, CommandResponse<Event>> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandHandler.class);
     private final Bank bank;
 
     public Pay(Bank bank) {
@@ -18,13 +24,34 @@ public class Pay implements CommandHandler<PayCommand, CommandResponse<Event>> {
     public CommandResponse<Event> execute(PayCommand command) {
         Payment payment = new Payment(command.cardNumber(), command.expirationDate(), command.cypher(), command.cartId(), command.amount());
         var transaction = bank.pay(payment);
+        if (transaction.isPending()) {
+            ValidationRequested validationRequested = new ValidationRequested(
+                    transaction.status(),
+                    transaction.id(),
+                    transaction.redirectionUrl(),
+                    command.amount());
+            LOGGER.info("Transaction is pending: {}", validationRequested);
+            return new CommandResponse<>(validationRequested);
+        } else if (!transaction.hasSucceeded()) {
+            TransactionFailed failedTransaction = new TransactionFailed(
+                    transaction.status(),
+                    transaction.id());
+            LOGGER.info("Transaction failed: {}", failedTransaction);
+            return new CommandResponse<>(failedTransaction);
+        } else {
+            LOGGER.info("Transaction for cart transactionId {} succeeded, with transaction transactionId:{}", command.cartId(), transaction.id());
 
-        return null;
+            return new CommandResponse<>(new PaymentSucceeded(
+                    transaction.status(),
+                    transaction.id(),
+                    command.cartId(),
+                    command.amount()));
+        }
     }
 
     @Override
     public Class listenTo() {
-        return null;
+        return PayCommand.class;
     }
 
 
