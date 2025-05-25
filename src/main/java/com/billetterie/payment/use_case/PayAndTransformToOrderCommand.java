5a338464bb1885ab7216a75384b1e45d032190ec
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-25 21:47:57 +0200

    billetterie: 06.2 [unit test] for PayAndTransformToOrder command handler

--- src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrderCommand.java
+++ src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrderCommand.java
@@ -1,4 +1,6 @@
 package com.billetterie.payment.use_case;
 
-public record PayAndTransformToOrderCommand(String cartId, String cardNumber, String expirationDate, String cypher, float amount) {
+import com.billetterie.payment.common.cqrs.command.Command;
+
+public record PayAndTransformToOrderCommand(String cartId, String cardNumber, String expirationDate, String cypher, float amount) implements Command {
 }
