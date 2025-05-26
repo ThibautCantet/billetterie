3f64cd8964cd7ff820c735c84cc546ae00ff27b3
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-26 19:02:54 +0200

    billletterie: register CancelTransaction and AlertTransactionFailure in getCommandHandlers, register OrderNotCreatedListener and CancelTransactionFailedListener in getEventHandlers

--- src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
+++ src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
@@ -8,8 +8,13 @@ import com.billetterie.payment.common.cqrs.event.EventHandler;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBus;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBusFactory;
 import com.billetterie.payment.domain.Bank;
+import com.billetterie.payment.domain.CustomerSupport;
 import com.billetterie.payment.domain.Orders;
+import com.billetterie.payment.listener.CancelTransactionFailedListener;
+import com.billetterie.payment.listener.OrderNotCreatedListener;
 import com.billetterie.payment.listener.PaymentSucceededListener;
+import com.billetterie.payment.use_case.AlertTransactionFailure;
+import com.billetterie.payment.use_case.CancelTransaction;
 import com.billetterie.payment.use_case.Pay;
 import com.billetterie.payment.use_case.TransformToOrder;
 import org.springframework.stereotype.Service;
@@ -20,22 +25,28 @@ public class CommandBusFactory {
 
     private final Bank bank;
     private final Orders orders;
+    private final CustomerSupport customerSupport;
 
-    public CommandBusFactory(Bank bank, Orders orders) {
+    public CommandBusFactory(Bank bank, Orders orders, CustomerSupport customerSupport) {
         this.bank = bank;
         this.orders = orders;
+        this.customerSupport = customerSupport;
     }
 
     protected List<CommandHandler> getCommandHandlers() {
         return List.of(
                 new Pay(bank),
-                new TransformToOrder(orders)
+                new TransformToOrder(orders),
+                new CancelTransaction(bank),
+                new AlertTransactionFailure(customerSupport)
         );
     }
 
     protected List<EventHandler<? extends Event>> getEventHandlers() {
         return List.of(
-                new PaymentSucceededListener()
+                new PaymentSucceededListener(),
+                new OrderNotCreatedListener(),
+                new CancelTransactionFailedListener()
         );
     }
 
