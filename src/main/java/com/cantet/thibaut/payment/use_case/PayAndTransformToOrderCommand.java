48d0edf71074975d5e9b3f92656c98d86ed1b842
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-26 18:11:21 +0200

    billetterie: remove PayAndTransformToOrder use case replaced by Pay handler and PaymentSucceededListener listener

--- src/main/java/com/cantet/thibaut/payment/use_case/PayAndTransformToOrderCommand.java
+++
@@ -1,6 +0,0 @@
-package com.cantet.thibaut.payment.use_case;
-
-import com.cantet.thibaut.payment.common.cqrs.command.Command;
-
-public record PayAndTransformToOrderCommand(String cartId, String cardNumber, String expirationDate, String cypher, float amount) implements Command {
-}
