6075b2c511b1f07787bf227ed3b48ea8682eba83
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-26 18:10:09 +0200

    billetterie: implement commandBusDispatcher for PaymentController, add Pay and TransformToOrder handler in getCommandHandlers, add PaymentSucceededListener in getEventHandlers in CommandBusFactory

--- src/main/java/com/cantet/thibaut/payment/common/cqrs/middleware/command/CommandBusFactory.java
+++ src/main/java/com/cantet/thibaut/payment/common/cqrs/middleware/command/CommandBusFactory.java
@@ -7,22 +7,41 @@ import com.cantet.thibaut.payment.common.cqrs.event.Event;
 import com.cantet.thibaut.payment.common.cqrs.event.EventHandler;
 import com.cantet.thibaut.payment.common.cqrs.middleware.event.EventBus;
 import com.cantet.thibaut.payment.common.cqrs.middleware.event.EventBusFactory;
+import com.cantet.thibaut.payment.domain.Bank;
+import com.cantet.thibaut.payment.domain.Orders;
+import com.cantet.thibaut.payment.listener.PaymentSucceededListener;
+import com.cantet.thibaut.payment.use_case.AlertTransactionFailure;
+import com.cantet.thibaut.payment.use_case.CancelTransaction;
+import com.cantet.thibaut.payment.use_case.Pay;
+import com.cantet.thibaut.payment.use_case.TransformToOrder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommandBusFactory {
 
 
-    public CommandBusFactory() {
+    private final Bank bank;
+    private final Orders orders;
+    private final CancelTransaction cancelTransaction;
+    private final AlertTransactionFailure alertTransactionFailure;
+
+    public CommandBusFactory(Bank bank, Orders orders, CancelTransaction cancelTransaction, AlertTransactionFailure alertTransactionFailure) {
+        this.bank = bank;
+        this.orders = orders;
+        this.cancelTransaction = cancelTransaction;
+        this.alertTransactionFailure = alertTransactionFailure;
     }
 
     protected List<CommandHandler> getCommandHandlers() {
         return List.of(
+                new Pay(bank),
+                new TransformToOrder(orders, cancelTransaction, alertTransactionFailure)
         );
     }
 
     protected List<EventHandler<? extends Event>> getEventHandlers() {
         return List.of(
+                new PaymentSucceededListener()
         );
     }
 
