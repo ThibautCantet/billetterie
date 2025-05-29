4222f1385c4a4b9046a799f5b94f65310db23f7a
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 18:42:29 +0200

    billetterie: 07 implement Pay handler

--- src/main/java/com/billetterie/payment/use_case/Pay.java
+++ src/main/java/com/billetterie/payment/use_case/Pay.java
@@ -5,6 +5,10 @@ import com.billetterie.payment.common.cqrs.command.CommandResponse;
 import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.Bank;
 import com.billetterie.payment.domain.Payment;
+import com.billetterie.payment.domain.PaymentStatus;
+import com.billetterie.payment.domain.PaymentSucceeded;
+import com.billetterie.payment.domain.TransactionFailed;
+import com.billetterie.payment.domain.ValidationRequested;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,14 +22,18 @@ public class Pay implements CommandHandler<PayCommand, CommandResponse<Event>> {
     public CommandResponse<Event> execute(PayCommand command) {
         Payment payment = new Payment(command.cartId(), command.cardNumber(), command.expirationDate(), command.cypher(), command.amount());
         var transaction = bank.pay(payment);
-        //TODO: switch on transaction status and return
-        // ValidationRequested, TransactionFailed or PaymentSucceeded
-        return null;
+        if (transaction.isPending()) {
+            return new CommandResponse<>(new ValidationRequested(PaymentStatus.PENDING, transaction.id(), transaction.redirectionUrl(), command.amount()));
+        } else if (!transaction.hasSucceeded()) {
+            return new CommandResponse<>(new TransactionFailed(PaymentStatus.FAILED, transaction.id()));
+        } else {
+            return new CommandResponse<>(new PaymentSucceeded(PaymentStatus.SUCCESS, transaction.id(), command.cartId(), command.amount()));
+        }
     }
 
     @Override
     public Class listenTo() {
-        return null;
+        return PayCommand.class;
     }
 
 
