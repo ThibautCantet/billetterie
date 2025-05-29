164bd30e58ae577ba19c59278c8d594d617b1966
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 13:44:34 +0200

    billetterie: 08.1 implement CommandBusFactory for Pay handler in PaymentController

--- src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
+++ src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
@@ -7,24 +7,41 @@ import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.common.cqrs.event.EventHandler;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBus;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBusFactory;
+import com.billetterie.payment.domain.Bank;
+import com.billetterie.payment.domain.CustomerSupport;
+import com.billetterie.payment.domain.Orders;
+import com.billetterie.payment.listener.PaymentSucceededListener;
+import com.billetterie.payment.use_case.AlertTransactionFailure;
+import com.billetterie.payment.use_case.CancelTransaction;
+import com.billetterie.payment.use_case.Pay;
+import com.billetterie.payment.use_case.TransformToOrder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommandBusFactory {
 
+    private final Bank bank;
+    private final Orders orders;
+    private final CustomerSupport customerSupport;
 
-    public CommandBusFactory() {
+    public CommandBusFactory(Bank bank, Orders orders, CustomerSupport customerSupport) {
+        this.bank = bank;
+        this.orders = orders;
+        this.customerSupport = customerSupport;
     }
 
     protected List<CommandHandler> getCommandHandlers() {
         return List.of(
-                //TODO: add Pay and TransformToOrder handlers
+                new Pay(bank),
+                new TransformToOrder(orders, bank, customerSupport,
+                        new CancelTransaction(bank),
+                        new AlertTransactionFailure(customerSupport))
         );
     }
 
     protected List<EventHandler<? extends Event>> getEventHandlers() {
         return List.of(
-                //TODO: add PaymentSucceededListener listener
+                new PaymentSucceededListener()
         );
     }
 
