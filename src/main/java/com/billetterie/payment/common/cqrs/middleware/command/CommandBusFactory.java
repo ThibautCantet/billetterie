3e45aba1cb71b5c655b24c5818aac5bab3d6147a
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 18:51:38 +0200

    billetterie: 08.2 implement CommandBusFactory for Pay handler in PaymentController

--- src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
+++ src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
@@ -8,7 +8,12 @@ import com.billetterie.payment.common.cqrs.event.EventHandler;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBus;
 import com.billetterie.payment.common.cqrs.middleware.event.EventBusFactory;
 import com.billetterie.payment.domain.Bank;
+import com.billetterie.payment.domain.CustomerSupport;
 import com.billetterie.payment.domain.Orders;
+import com.billetterie.payment.infrastructure.service.EmailCustomerSupport;
+import com.billetterie.payment.listener.PaymentSucceededListener;
+import com.billetterie.payment.use_case.AlertTransactionFailure;
+import com.billetterie.payment.use_case.CancelTransaction;
 import com.billetterie.payment.use_case.Pay;
 import com.billetterie.payment.use_case.TransformToOrder;
 import org.springframework.stereotype.Service;
@@ -16,24 +21,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class CommandBusFactory {
 
-
     private final Bank bank;
     private final Orders orders;
+    private final CustomerSupport customerSupport;
 
-    public CommandBusFactory(Bank bank, Orders orders) {
+    public CommandBusFactory(Bank bank, Orders orders, CustomerSupport customerSupport) {
         this.bank = bank;
         this.orders = orders;
+        this.customerSupport = customerSupport;
     }
 
     protected List<CommandHandler> getCommandHandlers() {
         return List.of(
-                //TODO: add Pay and TransformToOrder (new CancelTransaction and AlertTransactionFailure(customerSupport)) handlers
+                new Pay(bank),
+                new TransformToOrder(orders, bank, customerSupport,
+                        new CancelTransaction(bank),
+                        new AlertTransactionFailure(customerSupport))
         );
     }
 
     protected List<EventHandler<? extends Event>> getEventHandlers() {
         return List.of(
-                //TODO: add PaymentSucceededListener listener
+                new PaymentSucceededListener()
         );
     }
 
--- src/main/java/com/billetterie/payment/listener/PaymentSucceededListener.java
+++ src/main/java/com/billetterie/payment/listener/PaymentSucceededListener.java
@@ -12,7 +12,6 @@ public class PaymentSucceededListener extends EventHandlerCommand<PaymentSucceed
 
     @Override
     public Command execute(PaymentSucceeded event) {
-        //TODO: then implement CommandBusFactory and PaymentController
         return new TransformToOrderCommand(event.transactionId(), event.cartId(), event.amount());
     }
 
