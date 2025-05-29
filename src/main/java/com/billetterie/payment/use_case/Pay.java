c2537d2df15c9379e4675c113a7fd2c5a9c6a6b1
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 10:40:36 +0200

    billetterie: 01 implement Pay use case

--- src/main/java/com/billetterie/payment/use_case/Pay.java
+++ src/main/java/com/billetterie/payment/use_case/Pay.java
@@ -3,7 +3,9 @@ package com.billetterie.payment.use_case;
 import com.billetterie.payment.domain.Bank;
 import com.billetterie.payment.domain.Payment;
 import com.billetterie.payment.domain.Transaction;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Pay {
     private final Bank bank;
 
@@ -12,7 +14,7 @@ public class Pay {
     }
 
     public Transaction execute(PayCommand command) {
-        return null;
+        return bank.pay(new Payment(command.cartId(), command.cardNumber(), command.expirationDate(), command.cypher(), command.amount()));
     }
 
 
