be7c579e8aefaf92ba9fbccaab9cb9eb2ae97a83
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 19:34:18 +0200

    billetterie: 11 [unit test] remove CancelTransaction and AlertTransactionFailure command handlers from TransformToOrder use case

--- src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
+++ src/main/java/com/billetterie/payment/common/cqrs/middleware/command/CommandBusFactory.java
@@ -32,6 +32,7 @@ public class CommandBusFactory {
     }
 
     protected List<CommandHandler> getCommandHandlers() {
+        //TODO: register CancelTransaction, AlertTransactionFailure handlers
         return List.of(
                 new PayCommandHandler(bank),
                 new TransformToOrderCommandHandler(orders, bank,
@@ -41,6 +42,7 @@ public class CommandBusFactory {
     }
 
     protected List<EventHandler<? extends Event>> getEventHandlers() {
+        //TODO: register OrderNotCreatedListener and CancelTransactionFailedListener listeners
         return List.of(
                 new PaymentSucceededListener()
         );
