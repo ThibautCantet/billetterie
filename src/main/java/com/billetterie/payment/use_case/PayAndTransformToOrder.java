2487af444012ac60c378bc66b8247dc55bcd2f3c
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-29 18:37:01 +0200

    billetterie: 06.4 implement PayAndTransformToOrder handler

--- src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrder.java
+++ src/main/java/com/billetterie/payment/use_case/PayAndTransformToOrder.java
@@ -4,8 +4,10 @@ import com.billetterie.payment.common.cqrs.command.CommandHandler;
 import com.billetterie.payment.common.cqrs.command.CommandResponse;
 import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.Bank;
-import com.billetterie.payment.domain.PayAndTransformToOrderResult;
+import com.billetterie.payment.domain.PaymentStatus;
 import com.billetterie.payment.domain.Transaction;
+import com.billetterie.payment.domain.TransactionFailed;
+import com.billetterie.payment.domain.ValidationRequested;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -28,31 +30,27 @@ public class PayAndTransformToOrder implements CommandHandler<PayAndTransformToO
         Transaction transaction = pay.execute(new PayCommand(command.cartId(), command.cardNumber(), command.expirationDate(), command.cypher(), command.amount()));
 
         if (transaction.isPending()) {
-            //TODO: replace payAndTransformToOrderResult by a ValidationRequested event
-            var pendingTransaction = PayAndTransformToOrderResult.pending(
+            var validationRequested = new ValidationRequested(
+                    PaymentStatus.PENDING,
                     transaction.id(),
                     transaction.redirectionUrl(),
                     command.amount());
-            LOGGER.info("Transaction is pending: {}", pendingTransaction);
-            return null;
+            LOGGER.info("Transaction is pending: {}", validationRequested);
+            return new CommandResponse<>(validationRequested);
         }
 
         if (!transaction.hasSucceeded()) {
-            //TODO: replace payAndTransformToOrderResult by a TransactionFailed event
             //TODO: then remove the PayAndTransformToOrderResult record
-            var failedTransaction = PayAndTransformToOrderResult.failed(
+            var transactionFailed = new TransactionFailed(
                     transaction.status(),
                     transaction.id());
-            LOGGER.info("Transaction failed: {}", failedTransaction);
-            return null;
+            LOGGER.info("Transaction failed: {}", transactionFailed);
+            return  new CommandResponse<>(transactionFailed);
         }
 
         LOGGER.info("Transaction for cart id {} succeeded, with transaction id:{}", command.cartId(), transaction.id());
 
-        //TODO: return the result of the transformation to order
-        var response = transformToOrder.execute(new TransformToOrderCommand(transaction.id(), command.cartId(), command.amount()));
-
-        return null;
+        return transformToOrder.execute(new TransformToOrderCommand(transaction.id(), command.cartId(), command.amount()));
     }
 
     @Override
