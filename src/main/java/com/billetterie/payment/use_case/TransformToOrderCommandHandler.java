05063eeb83422ab16721e8d01a611f4b9dba9a36
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-05-30 21:27:45 +0200

    billetterie: 06 implement transform and pay and transform command handler

--- src/main/java/com/billetterie/payment/use_case/TransformToOrderCommandHandler.java
+++ src/main/java/com/billetterie/payment/use_case/TransformToOrderCommandHandler.java
@@ -4,11 +4,10 @@ import com.billetterie.payment.common.cqrs.command.CommandHandler;
 import com.billetterie.payment.common.cqrs.command.CommandResponse;
 import com.billetterie.payment.common.cqrs.event.Event;
 import com.billetterie.payment.domain.CancelTransactionFailed;
-import com.billetterie.payment.domain.Bank;
-import com.billetterie.payment.domain.CustomerSupport;
 import com.billetterie.payment.domain.Order;
+import com.billetterie.payment.domain.OrderCreated;
+import com.billetterie.payment.domain.OrderNotCreated;
 import com.billetterie.payment.domain.Orders;
-import com.billetterie.payment.domain.PayAndTransformToOrderResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -18,15 +17,11 @@ public class TransformToOrderCommandHandler implements CommandHandler<TransformT
     private static final Logger LOGGER = LoggerFactory.getLogger(TransformToOrderCommandHandler.class);
 
     private final Orders orders;
-    private final Bank bank;
-    private final CustomerSupport customerSupport;
     private final CancelTransactionCommandHandler cancelTransactionCommandHandler;
     private final AlertTransactionFailureCommandHandler alertTransactionFailureCommandHandler;
 
-    public TransformToOrderCommandHandler(Orders orders, Bank bank, CustomerSupport customerSupport, CancelTransactionCommandHandler cancelTransactionCommandHandler, AlertTransactionFailureCommandHandler alertTransactionFailureCommandHandler) {
+    public TransformToOrderCommandHandler(Orders orders, CancelTransactionCommandHandler cancelTransactionCommandHandler, AlertTransactionFailureCommandHandler alertTransactionFailureCommandHandler) {
         this.orders = orders;
-        this.bank = bank;
-        this.customerSupport = customerSupport;
         this.cancelTransactionCommandHandler = cancelTransactionCommandHandler;
         this.alertTransactionFailureCommandHandler = alertTransactionFailureCommandHandler;
     }
@@ -44,29 +39,28 @@ public class TransformToOrderCommandHandler implements CommandHandler<TransformT
                 LOGGER.info("Transaction cancelled: {}", command.transactionId());
             }
 
-            //TODO: replace payAndTransformToOrderResult by a OrderNotCreated event
-            var failed = PayAndTransformToOrderResult.failed(
+            var orderNotCreated = new OrderNotCreated(
                     command.transactionId(),
-                    getErrorCartUrl(command.cartId(), command.amount()));
-            LOGGER.info("Cart not transformed into order and redirect to empty cart: {}", failed);
+                    command.amount(),
+                    getErrorCartUrl(command.cartId(), command.amount()),
+                    command.cartId());
+            LOGGER.info("Cart not transformed into order and redirect to empty cart: {}", orderNotCreated);
 
-            return null;
+            return new CommandResponse<>(orderNotCreated);
         }
 
         LOGGER.info("Cart transformed to order: {}", order.id());
-        //TODO: replace payAndTransformToOrderResult by a OrderCreated event
-        //TODO: use OrderCreated.of
-        PayAndTransformToOrderResult.succeeded(
+        var orderCreated = OrderCreated.of(
                 command.transactionId(),
                 order.id(),
                 command.amount());
 
-        return null;
+        return new CommandResponse<>(orderCreated);
     }
 
     @Override
     public Class listenTo() {
-        return null;
+        return TransformToOrderCommand.class;
     }
 
     public static String getErrorCartUrl(String cartId, float amount) {
